7402bcaa0a86ff539bc7e702e4d9875430673639
G0gss <123230512+G0gss@users.noreply.github.com>
2023-06-17 09:44:04 +0300

    патерн Фабрика.py

--- патерн Фабрика.py
+++ патерн Фабрика.py
@@ -6,40 +6,39 @@ class GUIFactory: # Абстрактный класс GUI фабрики
     def create_checkbox(self):
         pass
         
-class WinFactory(GUIFactory): # Конкретная Win фабрика, которая создает Win кнопки и чекбоксы
+class WinFactory(GUIFactory): 
     def create_button(self):
         return WinButton()
     
     def create_checkbox(self):
         return WinCheckbox()
         
-class MacFactory(GUIFactory): # Конкретная Mac фабрика, которая создает Mac кнопки и чекбоксы
-    def create_button(self):
+class MacFactory(GUIFactory): 
         return MacButton()
     
     def create_checkbox(self):
         return MacCheckbox()
         
-class Button: # Абстрактный класс для кнопок
+class Button:
     def paint(self):
         pass
         
-class Checkbox: # Абстрактный класс для чекбоксов
+class Checkbox:
     def paint(self):
         pass
         
-class WinButton(Button): # Класс для создания Win кнопок
+class WinButton(Button): 
     def paint(self):
         return 'Рисование кнопки выигрыша'
     
-class MacButton(Button): # Класс для создания Mac кнопок
+class MacButton(Button):
     def paint(self):
         return 'Рисование кнопки Mac'
         
-class WinCheckbox(Checkbox): # Класс для создания Win чекбоксов
+class WinCheckbox(Checkbox):
      def paint(self):
         return 'Устанавливаем флажок "Выиграть"'
     
-class MacCheckbox(Checkbox): # Класс для создания Mac чекбоксов
+class MacCheckbox(Checkbox):
     def paint(self):
         return 'Рисование флажка Mac'
